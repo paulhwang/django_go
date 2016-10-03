830968942c8ec24ca128f9855b994a40f7a0a1a0
paul.hwang <phwang@gmail.com>
2016-10-03 22:33:14 +0800

    model

--- go_server/models.py
+++ go_server/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class AccountObject(models.Model):
+	account = models.CharField(max_length = 32)
+	password = models.CharField(max_length = 32)
+	name = models.TextField()
