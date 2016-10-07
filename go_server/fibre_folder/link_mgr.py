78b1e03f33a4ce6918c949cc23782df1d25a0e17
paul.hwang <phwang@gmail.com>
2016-10-07 21:04:11 +0800

    link mgr

--- go_server/fibre_folder/link_mgr.py
+++ go_server/fibre_folder/link_mgr.py
@@ -1,9 +1,44 @@
+import go_server.util_folder.queue
+
 def malloc(fibre_val):
     return LinkMgrClass(fibre_val)
 
 class LinkMgrClass(object):
     def __init__(self, fibre_val):
         self.theFibreObject = fibre_val;
+        self.theGlobalLinkId = 10;
+        self.thePoolHead = 0;
+        self.thePoolSize = 0;
+        self.theLinkQueue = go_server.util_folder.queue.malloc();
 
     def className(self):
         return "LinkMgrClass"
+
+    def poolHead(self):
+        return self.thePoolHead
+
+    def setPoolHead(self, val):
+        self.thePoolHead = val
+
+    def poolSize(self):
+        return self.thePoolSize
+
+    def incrementPoolSize(self):
+        self.thePoolSize += 1
+
+    def decrementPoolSize(self):
+        self.thePoolSize -= 1
+
+    def abendIt(self):
+        i = 0
+        p = self.poolHead()
+        while p != 0:
+            p = p.next()
+            i += 1
+
+        if i != self.poolSize():
+            self.abend("abendIt", "size=" + self.poolSize() + " i=" + i)
+
+        if self.poolSize() > 5:
+            self.abend("abendIt", "size=" + self.poolSize())
+
