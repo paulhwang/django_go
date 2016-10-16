9c8ae6950d23854d1e00e64682dacd811992379a
paul.hwang <phwang@gmail.com>
2016-10-16 18:13:34 +0800

    ring

--- go_server/project_modules/util_modules/ring.py
+++ go_server/project_modules/util_modules/ring.py
@@ -3,10 +3,23 @@ def malloc(util_val):
 
 class RingClass(object):
     def __init__(self, util_val):
-        self.theUtiObject = util_val
+        self.theUtilObject = util_val
 
     def className(self):
         return "RingClass"
 
-    def utiObject(self):
-        return theUtiObject
+    def utilObject(self):
+        return theUtilObject
+
+    def rootObject(self):
+        return self.utilObject().rootObject()
+
+    def debug(self, bool_val, str1, str2, str3 = "", str4 = "", str5 = "", str6 = "", str7 = "", str8 = "", str9 = "", str10 = "", str11 = ""):
+        if bool_val:
+            self.logit(str1, str2, str3, str4, str5, str6, str7, str8, str9, str10, str11)
+
+    def logit(self, str1, str2, str3 = "", str4 = "", str5 = "", str6 = "", str7 = "", str8 = "", str9 = "", str10 = "", str11 = ""):
+        self.rootObject().logit(self.className() + "." + str1 + "() ", str2, str3, str4, str5, str6, str7, str8, str9, str10, str11)
+
+    def abend(self, str1, str2, str3 = "", str4 = "", str5 = "", str6 = "", str7 = "", str8 = "", str9 = "", str10 = "", str11 = ""):
+        self.rootObject().abend(self.className() + "." + str1 + "() ", str2, str3, str4, str5, str6, str7, str8, str9, str10, str11)
