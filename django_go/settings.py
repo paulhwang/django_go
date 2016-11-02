86599c52fd4c1f16081c18bc7b760b90233229b5
Paul Hwang <paul.hwang@getac.com.tw>
2016-11-02 10:09:36 +0800

    ip

--- django_go/settings.py
+++ django_go/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't)6#-48d92pmw@cs^jig0wzboa!mqhfd=vk&dfia(mmmsyysp0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.168.80.187']
 
 
 # Application definition
