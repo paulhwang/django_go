f39bbbdbda376f17b003ac57cb52b155e6549c19
paul.hwang (黃文樸 - Getac) <paul.hwang@getac.com.tw>
2016-11-20 23:55:13 +0800

    log

--- go_server/phwang_modules/phwang.py
+++ go_server/phwang_modules/phwang.py
@@ -1,4 +1,3 @@
-import go_server.phwang_modules.util_modules.util
 import go_server.phwang_modules.fabric_modules.fabric
 import go_server.phwang_modules.port_modules.port
 
@@ -16,7 +15,6 @@ def malloc_port(phwang_val):
 
 class RootClass(object):
     def __init__(self):
-        self.theUtilObject = malloc_util(self)
         self.theFarbricObject = malloc_farbric(self)
         self.thePortObject = malloc_port(self)
         self.debug(True, "init__", "")
@@ -53,7 +51,47 @@ class RootClass(object):
         self.ABEND(self.objectName() + "." + str1 + "() ", str2, str3, str4, str5, str6, str7, str8, str9, str10, str11)
 
     def LOG_IT(self, str1, str2, str3 = "", str4 = "", str5 = "", str6 = "", str7 = "", str8 = "", str9 = "", str10 = "", str11 = ""):
-        go_server.phwang_modules.util_modules.util.utilLogit(str1 + str2, str3, str4, str5, str6, str7, str8, str9, str10, str11)
+        go_server.phwang_modules.phwang.utilLogit(str1 + str2, str3, str4, str5, str6, str7, str8, str9, str10, str11)
 
     def ABEND(self, str1, str2, str3 = "", str4 = "", str5 = "", str6 = "", str7 = "", str8 = "", str9 = "", str10 = "", str11 = ""):
-        go_server.phwang_modules.util_modules.util.utilAbend(str1 + str2, str3, str4, str5, str6, str7, str8, str9, str10, str11)
+        go_server.phwang_modules.phwang.utilAbend(str1 + str2, str3, str4, str5, str6, str7, str8, str9, str10, str11)
+
+
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+def utilLogit(str12, str3 = "", str4 = "", str5 = "", str6 = "", str7 = "", str8 = "", str9 = "", str10 = "", str11 = ""):
+    if  str3 == "":
+        logger.error(str12)
+        return
+    if  str4 == "":
+        logger.error(str12, str3)
+        return
+    if  str5 == "":
+        logger.error(str12, str3, str4)
+        return
+    if  str6 == "":
+        logger.error(str12, str3, str4, str5)
+        return
+    if  str7 == "":
+        logger.error(str12, str3, str4, str5, str6)
+        return
+    if  str8 == "":
+        logger.error(str12, str3, str4, str5, str6, str7)
+        return
+    if  str9 == "":
+        logger.error(str12, str3, str4, str5, str6, str7, str8)
+        return
+    if  str10 == "":
+        logger.error(str12, str3, str4, str5, str6, str7, str8, str9)
+        return
+    if  str11 == "":
+        logger.error(str12, str3, str4, str5, str6, str7, str8, str9, str10)
+        return
+    logger.error(str12, str3, str4, str5, str6, str7, str8, str9, str10, str11)
+
+
+def utilAbend(str12, str3 = "", str4 = "", str5 = "", str6 = "", str7 = "", str8 = "", str9 = "", str10 = "", str11 = ""):
+    utilLogit("Abend " + str12, str3, str4, str5, str6, str7, str8, str9, str10, str11)
